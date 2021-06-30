fdfc57e34cda1fe4926e18481155f012b9bffe83
Vadim <vtimofeev223@gmail.com>
2021-06-30 10:06:01 +0300

    Added S3 Filestorage settings on Tenants screen

--- vue/manager.js
+++ vue/manager.js
@@ -20,4 +20,21 @@ export default {
       },
     ]
   },
+  getAdminTenantTabs () {
+    return [
+      {
+        tabName: 's3-filestorage',
+        paths: [
+          'id/:id/s3-filestorage',
+          'search/:search/id/:id/s3-filestorage',
+          'page/:page/id/:id/s3-filestorage',
+          'search/:search/page/:page/id/:id/s3-filestorage',
+        ],
+        title: 'S3FILESTORAGE.LABEL_SETTINGS_TAB',
+        component () {
+          return import('src/../../../S3Filestorage/vue/components/S3FilestorageAdminSettingsPerTenant')
+        }
+      }
+    ]
+  },
 }
